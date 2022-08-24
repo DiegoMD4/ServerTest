f3172adc4ad565b9cf20550c728515b08cd13035
Diego Montoya <diegolink4@hotmail.com>
2022-08-23 20:07:03 -0600

    Subiendo post a Heroku

--- src/1.0.0/routes/post.js
+++ src/1.0.0/routes/post.js
@@ -2,7 +2,7 @@ const express= require('express');
 const router= express.Router();
 const config= require('../lib/config');
 const sql = require('mssql');
-const PostModule = require('../class/Post')
+const PostModule = require('../class/post')
 
 router.get('/post',async(req,res)=>{
     try {
